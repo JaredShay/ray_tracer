a71139f64f958101afe94bd5d60420748ee3f189
Jared Shay <jared.shay@gmail.com>
2018-11-20 15:23:45 -0800

    Add test output image.
    This is just a test to ensure image generation works in a usable way. It
    adds a height and width val to the image struct as well as the fn
    `pixel_at_mut` which returns a mutable Pixel.

--- src/image.rs
+++ src/image.rs
@@ -1,19 +1,31 @@
 use pixels::{Pixel};
 
 pub struct Image<T: Pixel> {
-    buffer: Vec<T>,
+    width: u32,
+    height: u32,
+    pub buffer: Vec<T>,
 }
 
 // The T: Clone contstaint is so when the buffer is initialized the pixel can
 // be cloned to fill the buffer.
 impl<T: Pixel> Image<T> where T: Clone {
-    pub fn new(length: usize) -> Image<T> {
+    pub fn new(width: u32, height: u32) -> Image<T> {
         Image {
-            buffer: vec![T::new(); length]
+            width: width,
+            height: height,
+            buffer: vec![T::new(); (width * height) as usize]
         }
     }
 
     pub fn raw_buffer(&self) -> Vec<u8> {
         self.buffer.iter().flat_map(|pixel| pixel.values()).collect()
+    }
+
+    pub fn pixel_at_mut(&mut self, x: u32, y: u32) -> Option<&mut T> {
+        if x >= self.width || y >= self.height {
+            None
+        } else {
+            Some(&mut self.buffer[(x + y * self.width) as usize])
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,29 @@ extern crate image as extern_image;
 use std::path::Path;
 
 mod pixels;
-use pixels::{RGBAPixel};
+use pixels::{RGBPixel};
 
 mod image;
 use image::{Image};
 
 const WIDTH: u32 = 800;
 const HEIGHT: u32 = 600;
-const PIXELS_SIZE: usize = (WIDTH * HEIGHT) as usize;
 
 fn main() {
-    let path = Path::new("out/test.png");
+    let path = Path::new("out/result.png");
 
-    let image: Image<RGBAPixel> = Image::new(PIXELS_SIZE);
+    let mut image: Image<RGBPixel> = Image::new(WIDTH, HEIGHT);
 
-    extern_image::save_buffer(path, &image.raw_buffer(), WIDTH, HEIGHT, extern_image::RGBA(8)).unwrap()
+    for i in 0..WIDTH {
+        for j in 0..HEIGHT {
+            let mut pixel = image.pixel_at_mut(i, j).unwrap();
+
+            pixel.r = (((1.0 / WIDTH as f32) * 255.0) * i as f32) as u8;
+            pixel.g = (((1.0 / HEIGHT as f32) * 255.0) * (HEIGHT as f32 - j as f32)) as u8;
+            pixel.b = (0.3 * 255.0) as u8;
+        }
+    }
+
+    extern_image::save_buffer(
+        path, &image.raw_buffer(), WIDTH, HEIGHT, extern_image::RGB(8)).unwrap()
 }
